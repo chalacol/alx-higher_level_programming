9c4d99a44dc1fa5a91ea0f18564880e93f6d0b5e
chalacol <ese.chala.amenu@gmail.com>
2021-12-21 06:15:43 -0800

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" read a file """
+"""defining read_file function"""
 
 
 def read_file(filename=""):
-    with open(filename,  encoding='utf-8') as f:
+    """reads filename with utf-8"""
+    with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")

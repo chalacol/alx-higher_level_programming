fee3f9572e635d8b85cef7a24212cea932a1826c
chalacol <ese.chala.amenu@gmail.com>
2022-02-28 03:15:19 -0800

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// This script prints 3 lines using an array of string and a loop
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let line = 0; line < lines.length; ++line) {
+    console.log(lines[line]);
+}

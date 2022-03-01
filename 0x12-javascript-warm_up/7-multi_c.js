7268544f7ed2bef1ccb05ee5043dc063baee2828
chalacol <ese.chala.amenu@gmail.com>
2022-02-28 22:35:53 -0800

    I love C updated

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-// This script prints x times "C is fun"
-if ((isNaN(process.argv[2])) === false) {
-    for (let num = 0; num < parseInt(process.argv[2]); num++){
-	console.log('C is fun');
-    }
-	} else {
-	    console.log('Missing number of occurrences');
-	}
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}

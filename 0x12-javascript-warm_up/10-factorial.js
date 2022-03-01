7734a2bf003bfdf18e4133b12c8795fd8a4bf4f5
chalacol <ese.chala.amenu@gmail.com>
2022-02-28 22:51:34 -0800

    factorial updated

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-// The script computes and prints a factorial
-function factorial(a) {
-    if (a === 1) {
-	return 1;
-    } else {
-	return a * factorial(a - 1);
-    }
+const firstArg = process.argv[2];
+function factorial (firstArg) {
+  if (isNaN(firstArg) || firstArg === 1) {
+    return (1);
+  } else {
+    return (firstArg * factorial(firstArg - 1));
+  }
 }
-if (isNaN(process.argv[2])) {
-    console.log(factorial(1));
-} else if (parseInt(process.argv[2]) >= 1) {
-    console.log(factorial(parseInt(process.argv[2])));
-}
+console.log(factorial(parseInt(firstArg)));

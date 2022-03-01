e46cc2217af8c0219470d89c1ca3cb92dde9beb4
chalacol <ese.chala.amenu@gmail.com>
2022-02-28 22:54:42 -0800

    add updated

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// The script adds 2 integers and the function is visible from outside
 exports.add = function (a, b) {
-    return (parseInt(a) + parseInt(b));
-}
+  return a + b;
+};

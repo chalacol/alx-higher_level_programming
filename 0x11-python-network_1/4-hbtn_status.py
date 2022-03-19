2b3a33c567a3dc80f0f60d5e4487f7ebf5b639a2
chalacol <ese.chala.amenu@gmail.com>
2022-03-18 19:38:37 -0700

    status 2 updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-script that fetches https://alx-intranet.hbtn.io/status
-"""
+"""fetches https://intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
-    result = requests.get("https://intranet.hbtn.io/status")
-
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(result.text)))
-    print("\t- content: {}".format(result.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))

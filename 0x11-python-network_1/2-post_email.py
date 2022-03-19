2654deab28fbda63ba77a9aa8b5bdb7f02a2a534
chalacol <ese.chala.amenu@gmail.com>
2022-03-18 19:33:33 -0700

    POST an email updated

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""
-script that takes in a URL and an email, sends a POST request
-to the passed URL with the email as a parameter
+"""A script that:
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a parameter
+- displays the body of the response
 """
 import sys
-import urllib.request
 import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
-    url_passed = sys.argv[1]
-    email_passed = {'email': sys.argv[2] }
-    data = urllib.parse.urlencode(email_passed).encode("ascii")
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url_passed, data) 
-    with urllib.request.urlopen(url_passed) as req:
-        print(req.read().decode('utf-8'))
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))

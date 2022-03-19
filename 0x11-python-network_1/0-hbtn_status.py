9c75de7442d6f06ccb031f1f6a97dd2a9099ec8f
chalacol <ese.chala.amenu@gmail.com>
2022-03-18 19:25:27 -0700

    SStatus updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://intranet.hbtn.io/status.
+- uses urlib package
 """
-Python script that fetches https://alx-intranet.hbtn.io/status
-"""
-import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
-        read_res = res.read()
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
-        print('\t - type: {}'.format(type(read_res)))
-        print('\t - content: {}'.format(read_res))
-        print('\t - utf8 content: {}'.format(read_res.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))

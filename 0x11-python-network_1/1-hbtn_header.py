7eff99076307d1981032b4ddfdb0b568650760f9
chalacol <ese.chala.amenu@gmail.com>
2022-03-18 19:29:06 -0700

    Response header value updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,8 @@ import sys
 import urllib.request
 
 if __name__ == "__main__":
-    url_passed = sys.argv[1]
-    with urllib.request.urlopen(url_passed) as req:
-        print(req.headers.get("X-Request-Id"))
+ url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
